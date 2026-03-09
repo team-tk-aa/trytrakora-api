f02c4931e9a9a23bed7fccda77d0b46856fc53fb
team-tk-aa <team.tk.aa@gmail.com>
2026-03-10 00:06:45 +0530

    fix3

--- post-build.js
+++ post-build.js
@@ -1,12 +1,12 @@
-import { copy } from 'fs-extra';
-import { join } from 'path';
+const fs = require('fs-extra');
+const path = require('path');
 
 // Source and destination paths
-const sourceDir = join(__dirname, './mails');
-const destDir = join(__dirname, './build/mails');
+const sourceDir = path.join(__dirname, './mails');
+const destDir = path.join(__dirname, './build/mails');
 
 // Copy the source folder to the destination
-copy(sourceDir, destDir, (err) => {
+fs.copy(sourceDir, destDir, (err) => {
   if (err) {
     console.error('Error copying folder:', err);
   } else {
