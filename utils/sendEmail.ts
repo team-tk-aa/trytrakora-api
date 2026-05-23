c51d3281b9e7719cd1081af792b09f21ecbd588f
team-tk-aa <team.tk.aa@gmail.com>
2026-05-23 20:57:41 +0530

    email fix

--- utils/sendEmail.ts
+++ utils/sendEmail.ts
@@ -15,12 +15,26 @@ interface EmailOptions {
 const sendEmail = async (options: EmailOptions): Promise<void> => {
     const transporter: Transporter = nodeMailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: parseInt(process.env.SMTP_PORT || '465'),
-        secure: true,
+        port: Number(process.env.SMTP_PORT),
+
+        secure: false,
+        requireTLS: true,
+
         auth: {
             user: process.env.SMTP_MAIL,
             pass: process.env.SMTP_PASSWORD
-        }
+        },
+
+        tls: {
+            rejectUnauthorized: false
+        },
+
+        connectionTimeout: 100000,
+        greetingTimeout: 100000,
+        socketTimeout: 100000,
+
+        logger: true,
+        debug: true
     });
 
     let { email, subject, template, data, cc, bcc } = options;
